8c77afe4a642ea3a7903baeb336b6becc46cd6f2
kimmyko <vanntharady2010@gmail.com>
2021-06-03 19:11:15 -0500

    test4

--- assets/js/email_send.js
+++ assets/js/email_send.js
@@ -1,45 +1,73 @@
 window.addEventListener("DOMContentLoaded", function () {
   // get the form elements defined in your form HTML above
 
-  var form = document.getElementById("contact");
+  var eventsContact = document.getElementById("eventsContact");
   // var button = document.getElementById("my-form-button");
-  var status = document.getElementById("form-submit");
+  var btn = document.getElementById("form-submit");
+  var senderEmail = eventsContact.email.value;
+  var senderName = eventsContact.name.value;
+  var senderMessage = document.getElementById("comments").value;
 
-  // Success and Error functions for after the form is submitted
+  eventsContact.addEventListener("submit", function (ev) {
+      Email.send({
+        SecureToken: "cbc604db-b1ee-44ca-b2f8-960e9c0575af",
+        To: "testcsausa@gmail.com",
+        From: senderEmail,
+        Subject: "Contact Us by Our Prospective Member",
+        Body: "Name:" + senderName + "<br/>Email: " + senderEmail + "<br/>Comments: " + senderMessage
+      }).then(msg => alert("Your message was successfully sent"))
+      $("#eventsContact").trigger("reset");
+      // window.location.reload();
+    });
 
-  function success() {
-    form.reset();
-    status.classList.add("success");
-    status.innerHTML = "Thanks!";
-  }
 
-  function error() {
-    status.classList.add("error");
-    status.innerHTML = "Oops! There was a problem.";
-  }
+  })
 
-  // handle the form submission event
 
-  form.addEventListener("submit", function (ev) {
-    ev.preventDefault();
-    var data = new FormData(form);
-    ajax(form.method, form.action, data, success, error);
-  });
-});
 
-// helper function for sending an AJAX request
 
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
+// window.addEventListener("DOMContentLoaded", function () {
+//   // get the form elements defined in your form HTML above
+
+//   var form = document.getElementById("contact");
+//   // var button = document.getElementById("my-form-button");
+//   var status = document.getElementById("form-submit");
+
+//   // Success and Error functions for after the form is submitted
+
+//   function success() {
+//     form.reset();
+//     status.classList.add("success");
+//     status.innerHTML = "Thanks!";
+//   }
+
+//   function error() {
+//     status.classList.add("error");
+//     status.innerHTML = "Oops! There was a problem.";
+//   }
+
+//   // handle the form submission event
+
+//   form.addEventListener("submit", function (ev) {
+//     ev.preventDefault();
+//     var data = new FormData(form);
+//     ajax(form.method, form.action, data, success, error);
+//   });
+// });
+
+// // helper function for sending an AJAX request
+
+// function ajax(method, url, data, success, error) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open(method, url);
+//   xhr.setRequestHeader("Accept", "application/json");
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//     if (xhr.status === 200) {
+//       success(xhr.response, xhr.responseType);
+//     } else {
+//       error(xhr.status, xhr.response, xhr.responseType);
+//     }
+//   };
+//   xhr.send(data);
+// }
---
+++ assets/js/googleMap.js
@@ -0,0 +1,71 @@
+let map, popup,popup2, Popup;
+
+/** Initializes the map and the custom popup. */
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.0902, lng: -95.7129 },
+    zoom: 3,
+  });
+
+  /**
+   * A customized popup on the map.
+   */
+  class Popup extends google.maps.OverlayView {
+    constructor(position, content) {
+      super();
+      this.position = position;
+      content.classList.add("popup-bubble");
+      // This zero-height div is positioned at the bottom of the bubble.
+      const bubbleAnchor = document.createElement("div");
+      bubbleAnchor.classList.add("popup-bubble-anchor");
+      bubbleAnchor.appendChild(content);
+      // This zero-height div is positioned at the bottom of the tip.
+      this.containerDiv = document.createElement("div");
+      this.containerDiv.classList.add("popup-container");
+      this.containerDiv.appendChild(bubbleAnchor);
+      // Optionally stop clicks, etc., from bubbling up to the map.
+      Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
+    }
+    /** Called when the popup is added to the map. */
+    onAdd() {
+      this.getPanes().floatPane.appendChild(this.containerDiv);
+    }
+    /** Called when the popup is removed from the map. */
+    onRemove() {
+      if (this.containerDiv.parentElement) {
+        this.containerDiv.parentElement.removeChild(this.containerDiv);
+      }
+    }
+    /** Called each frame when the popup needs to draw itself. */
+    draw() {
+      const divPosition = this.getProjection().fromLatLngToDivPixel(
+        this.position
+      );
+      // Hide the popup when it is far out of view.
+      const display =
+        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
+          ? "block"
+          : "none";
+
+      if (display === "block") {
+        this.containerDiv.style.left = divPosition.x + "px";
+        this.containerDiv.style.top = divPosition.y + "px";
+      }
+
+      if (this.containerDiv.style.display !== display) {
+        this.containerDiv.style.display = display;
+      }
+    }
+  }
+  popup2 = new Popup(
+    new google.maps.LatLng(40.7128,-74.0060),
+    document.getElementById("content2")
+  );
+
+  popup = new Popup(
+    new google.maps.LatLng(36.7783,-119.4179),
+    document.getElementById("content")
+  );
+  popup.setMap(map);
+  popup2.setMap(map);
+}
